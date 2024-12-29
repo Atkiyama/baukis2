9ae35ba578ba25b768a59e35493b3f2d022a33f6
Atkiyama <thisisatkiyama@gmail.com>
2024-12-30 00:37:00 +0900

    画面表示

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ rspecでテスト
 ```bash
 bundle exec rspec spec/以下パス
 ```
+
+データベースの作成
+```bash
+bin/rails db:create
+```
+
+アプリ起動
+
+```bash
+bin/rails s -b 0.0.0.0
+or
+docker compose exec web bin/rails s -b 0.0.0.0
+```
--- apps/baukis2/config/routes.rb
+++ apps/baukis2/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
-    resources :staff_members
   end
   namespace :admin do
     root 'top#index'
-    resources :staff_members
   end
   namespace :customer do
     root 'top#index'
